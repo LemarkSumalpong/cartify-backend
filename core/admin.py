b3ca3e7f8c94b856253bbc3088b2487467f6fa51
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-24 23:03:09 +0800

    test

--- core/admin.py
+++ core/admin.py
@@ -7,7 +7,7 @@ class CustomUserAdmin(UserAdmin):
     add_fieldsets = (
         (None, {
             'classes': ('wide',),
-            'fiels': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state', 'address', 'phone', 'is_staff', 'is_active',)}
+            'fields': ('username', 'email', 'first_name', 'last_name', 'password1', 'password2', 'city', 'state', 'address', 'phone', 'is_staff', 'is_active',)}
         ),
     )
 
