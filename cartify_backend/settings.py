ce60b24e76f0a4fd613b9e0a0b65122e920edf12
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-24 22:28:26 +0800

    added product models

--- cartify_backend/settings.py
+++ cartify_backend/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'shop_app'
 ]
 
 MIDDLEWARE = [
@@ -121,4 +122,4 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
-AUTH_USER_MODEL = "core.CustomUSer"
+AUTH_USER_MODEL = "core.CustomUser"
