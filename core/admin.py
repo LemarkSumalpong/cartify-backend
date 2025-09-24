b1f15ab20a891b87fcaf12a2aaf3c7ba979b769e
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-09-24 22:06:49 +0800

    added admin models

--- core/admin.py
+++ core/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
+from .models import CustomUser
 
 # Register your models here.
 class CustomUserAdmin(UserAdmin):
@@ -10,3 +11,4 @@ class CustomUserAdmin(UserAdmin):
         ),
     )
 
+admin.sites.register(CustomUser, CustomUserAdmin)
