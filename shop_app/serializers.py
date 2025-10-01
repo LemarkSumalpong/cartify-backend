6e2abde4e19636bd4e409cb9c21d24da17b03b06
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-01 21:34:14 +0800

    url

--- shop_app/serializers.py
+++ shop_app/serializers.py
@@ -4,4 +4,4 @@ from .models import Product
 class ProductSerializer(serializers.ModelSerializer):
     class Meta:
         model = Product
-        fields = ["id", "nanme", "slug", "image", "description", "category", "price"]
+        fields = ["id", "name", "slug", "image", "description", "category", "price"]
--- shop_app/urls.py
+++ shop_app/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path("products", views.products, name="products"),
+    path("products/", views.products, name="products"),
 ]
 
 # fecthing all_products: http://127.0.0.1:8000/products
