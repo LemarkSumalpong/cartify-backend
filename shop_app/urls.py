8b13aa629b5c34f96716a3b3d44d2a32c6e17da5
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-01 21:18:08 +0800

    url

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -5,4 +5,4 @@ urlpatterns = [
     path("products", views.products, name="products"),
 ]
 
-# fecthing all_products: http://127.0.0.1:8000/product/
+# fecthing all_products: http://127.0.0.1:8000/products
