69f111b184b0a48fa636e196303a9e24ac0f4397
lemarksenpai05 <sumalponglemark2@gmail.com>
2025-10-07 22:58:00 +0800

    serializer

--- shop_app/urls.py
+++ shop_app/urls.py
@@ -3,6 +3,7 @@ from . import views
 
 urlpatterns = [
     path("products/", views.products, name="products"),
+    path("product_detail/<slug:slug>", views.product_detail, name="product_detail")
 ]
 
 # fecthing all_products: http://127.0.0.1:8000/products
